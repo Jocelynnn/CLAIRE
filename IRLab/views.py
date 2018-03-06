489dea94bb5439d72772adc53068900ff7b20015
jocelynnn <bjie.jiang@gmail.com>
2018-03-06 16:15:32 -0600

    test server

--- IRLab/views.py
+++ IRLab/views.py
@@ -3,9 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .models import RetrievalMethod, Peformance, Code
 from .forms import bm25Form, jmForm, dpForm, plForm, adForm, ownRetrievalForm, ownRetrievalFormForDisplay
-import metapy
 import json
-import time
 import os
 from django.conf import settings
 import pytoml
@@ -25,95 +23,11 @@ import subprocess
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'home/post_list.html', {'posts': posts})
 
 
 def show_home(request):
-    return render(request, 'blog/home.html')
-
-
-@login_required
-def show_perfs(request):
-    rankers = RetrievalMethod.objects.filter(author=request.user)
-    perfs = Peformance.objects.filter(ranker__in=rankers)
-    table1 = PerfTable(Peformance.objects.filter(ranker__in=rankers, dataset='apnews'), prefix="ap")
-    table2 = PerfTable(Peformance.objects.filter(ranker__in=rankers, dataset='cranfield'), prefix="cr")
-
-    RequestConfig(request).configure(table1)
-    RequestConfig(request).configure(table2)
-    return render(request, 'evaluation/myperfs.html', {'table1': table1, 'table2': table2})
-
-
-def show_perfs_analysis(request):
-    if request.method == 'POST':
-        dataset = request.POST.get('dataset-dropdown', None)
-        ranker_name = request.POST.get('ranker-dropdown', None)
-
-        print(dataset)
-        print(ranker_name)
-
-        rankers = RetrievalMethod.objects.filter(name=ranker_name)
-        print(len(rankers))
-        perfs = Peformance.objects.filter(ranker__in=rankers, dataset=dataset)
-
-        params = []
-        data = []
-        if len(rankers) > 0:
-            ranker = rankers[0]
-            for key, value in ranker.__dict__.items():
-                key = str(key)
-                # print(key)
-                if key.startswith('p_'):
-                    params.append((key, str(key[2:]).upper()))
-
-            # only support ranker with one parameter
-            if len(params) > 1:
-                print('not valid')
-            else:
-
-                data.append([params[0][1], 'Map'])
-                param_map = [(perf.ranker.__getattribute__(params[0][0]), perf.map) for perf in perfs]
-                param_map = sorted(param_map, key=lambda pair: pair[0])
-
-                for pair in param_map:
-                    data.append([pair[0], pair[1]])
-
-
-    else:
-        # start from DL ranker
-        dataset = 'apnews'
-        ranker_name = 'Dirichlet Prior Smoothing'
-
-        rankers = RetrievalMethod.objects.filter(author=request.user, name='Dirichlet Prior Smoothing')
-        perfs = Peformance.objects.filter(ranker__in=rankers, dataset='apnews')
-
-        data = []
-        data.append(['Mu', 'Map'])
-        param_map = [(perf.ranker.p_mu, perf.map) for perf in perfs]
-        param_map = sorted(param_map, key=lambda pair: pair[0])
-        for pair in param_map:
-            data.append([pair[0], pair[1]])
-
-
-    # DataSource object
-    data_source = SimpleDataSource(data=data)
-    # Chart object
-    options = {
-        'title': 'Dirichlet Prior: Mu-Map',
-        'curveType': 'function',
-        'width': '900',
-        'height': '500',
-        'hAxis': {
-            'title': 'Mu'
-        },
-        'vAxis': {
-            'title': 'Map'
-        },
-    }
-    chart = LineChart(data_source, options=options)
-    context = {'chart': chart, 'dataset': dataset, 'ranker_name': ranker_name}
-
-    return render(request, 'application/charts.html', context)
+    return render(request, 'home/home.html')
 
 
 # @login_required
@@ -236,6 +150,90 @@ def create_search_engine(request, id):
                   {'ranker_name': ranker.name, 'id': id, 'ranker_id': ranker.ranker_id})
 
 
+
+@login_required
+def show_perfs(request):
+    rankers = RetrievalMethod.objects.filter(author=request.user)
+    perfs = Peformance.objects.filter(ranker__in=rankers)
+    table1 = PerfTable(Peformance.objects.filter(ranker__in=rankers, dataset='apnews'), prefix="ap")
+    table2 = PerfTable(Peformance.objects.filter(ranker__in=rankers, dataset='cranfield'), prefix="cr")
+
+    RequestConfig(request).configure(table1)
+    RequestConfig(request).configure(table2)
+    return render(request, 'evaluation/myperfs.html', {'table1': table1, 'table2': table2})
+
+
+def show_perfs_analysis(request):
+    if request.method == 'POST':
+        dataset = request.POST.get('dataset-dropdown', None)
+        ranker_name = request.POST.get('ranker-dropdown', None)
+
+        print(dataset)
+        print(ranker_name)
+
+        rankers = RetrievalMethod.objects.filter(name=ranker_name)
+        print(len(rankers))
+        perfs = Peformance.objects.filter(ranker__in=rankers, dataset=dataset)
+
+        params = []
+        data = []
+        if len(rankers) > 0:
+            ranker = rankers[0]
+            for key, value in ranker.__dict__.items():
+                key = str(key)
+                # print(key)
+                if key.startswith('p_'):
+                    params.append((key, str(key[2:]).upper()))
+
+            # only support ranker with one parameter
+            if len(params) > 1:
+                print('not valid')
+            else:
+
+                data.append([params[0][1], 'Map'])
+                param_map = [(perf.ranker.__getattribute__(params[0][0]), perf.map) for perf in perfs]
+                param_map = sorted(param_map, key=lambda pair: pair[0])
+
+                for pair in param_map:
+                    data.append([pair[0], pair[1]])
+
+
+    else:
+        # start from DL ranker
+        dataset = 'apnews'
+        ranker_name = 'Dirichlet Prior Smoothing'
+
+        rankers = RetrievalMethod.objects.filter(author=request.user, name='Dirichlet Prior Smoothing')
+        perfs = Peformance.objects.filter(ranker__in=rankers, dataset='apnews')
+
+        data = []
+        data.append(['Mu', 'Map'])
+        param_map = [(perf.ranker.p_mu, perf.map) for perf in perfs]
+        param_map = sorted(param_map, key=lambda pair: pair[0])
+        for pair in param_map:
+            data.append([pair[0], pair[1]])
+
+
+    # DataSource object
+    data_source = SimpleDataSource(data=data)
+    # Chart object
+    options = {
+        'title': 'Dirichlet Prior: Mu-Map',
+        'curveType': 'function',
+        'width': '900',
+        'height': '500',
+        'hAxis': {
+            'title': 'Mu'
+        },
+        'vAxis': {
+            'title': 'Map'
+        },
+    }
+    chart = LineChart(data_source, options=options)
+    context = {'chart': chart, 'dataset': dataset, 'ranker_name': ranker_name}
+
+    return render(request, 'application/charts.html', context)
+
 def search(request):
     if request.method == 'POST':
         ranker = RetrievalMethod.objects.get(id=request.POST.get('id', None))
@@ -301,7 +299,6 @@ def evaluate(request, id):
             target_script.write(target_ranker.read() + '\n')
             target_script.write(base)
     else:
-
         run_script = 'eval.py '
 
     # "python3 searcher.py config.toml "
@@ -355,39 +352,62 @@ def get_filled_form(ranker):
     return curt_form
 
 
-def generate_search_config(ranker):
-    dict = {}
-    dict['stop-words'] = "data/lemur-stopwords.txt"
-    dict['prefix'] = "."
-    dict['dataset'] = "apnews"
-    dict['corpus'] = "line.toml"
-    dict['index'] = "apnews-index"
-    dict['query-judgements'] = "qrels.txt"
+def generate_search_config(ranker,is_server = True):
+    if is_server:
+        dict = {}
+        dict['stop-words'] = "data/lemur-stopwords.txt"
+        dict['prefix'] = "."
+        dict['dataset'] = "wikipedia"
+        dict['corpus'] = "line.toml"
+        dict['index'] = "wikipedia-index"
 
-    dict['analyzers'] = [{'method': "ngram-word",
-                          'ngram': 1,
-                          'filter': "default-unigram-chain"}]
+        dict['analyzers'] = [{'method': "ngram-word",
+                              'ngram': 1,
+                              'filter': "default-unigram-chain"}]
 
-    dict['ranker'] = {'method': ranker.ranker_id_4_config}
 
-    config_params = ''
-    for key, value in ranker.__dict__.items():
-        key = str(key)
-        # print(key)
-        if key.startswith('p_'):
-            dict['ranker'][key[2:]] = round(float(value), 4)
-            config_params += key[2:] + '=' + str(round(float(value), 4)) + ","
+        config_params = ''
+        for key, value in ranker.__dict__.items():
+            key = str(key)
+            # print(key)
+            if key.startswith('p_'):
+                dict['ranker'][key[2:]] = round(float(value), 4)
+                config_params += key[2:] + '=' + str(round(float(value), 4)) + ","
 
-    # while True:
-    #     _num = random.randint(1, 10000)
-    #     file_name = 'c-'+str(_num) + '.toml'
-    #     base_dir = os.path.abspath(settings.BASE_DIR)
-    #     file_path = os.path.join(base_dir, file_name)
-    #     my_file = Path(file_path)
-    #     if not my_file.is_file():
-    #         with open(my_file, 'w+') as fout:
-    #             pytoml.dump(fout, dict)
-    #             break
+
+    else:
+        dict = {}
+        dict['stop-words'] = "data/lemur-stopwords.txt"
+        dict['prefix'] = "."
+        dict['dataset'] = "apnews"
+        dict['corpus'] = "line.toml"
+        dict['index'] = "apnews-index"
+        dict['query-judgements'] = "qrels.txt"
+
+        dict['analyzers'] = [{'method': "ngram-word",
+                              'ngram': 1,
+                              'filter': "default-unigram-chain"}]
+
+        dict['ranker'] = {'method': ranker.ranker_id_4_config}
+
+        config_params = ''
+        for key, value in ranker.__dict__.items():
+            key = str(key)
+            # print(key)
+            if key.startswith('p_'):
+                dict['ranker'][key[2:]] = round(float(value), 4)
+                config_params += key[2:] + '=' + str(round(float(value), 4)) + ","
+
+        # while True:
+        #     _num = random.randint(1, 10000)
+        #     file_name = 'c-'+str(_num) + '.toml'
+        #     base_dir = os.path.abspath(settings.BASE_DIR)
+        #     file_path = os.path.join(base_dir, file_name)
+        #     my_file = Path(file_path)
+        #     if not my_file.is_file():
+        #         with open(my_file, 'w+') as fout:
+        #             pytoml.dump(fout, dict)
+        #             break
     file_name = 'temp.toml'
     with open(file_name, 'w+') as fout:
         pytoml.dump(fout, dict)
@@ -435,92 +455,3 @@ def generate_eval_config(ranker, dataset):
     with open(file_name, 'w+') as fout:
         pytoml.dump(fout, dict)
     return file_name, config_params.strip(',')
-
-
-class Searcher:
-    """
-    Wraps the MeTA search engine and its rankers.
-    """
-
-    def __init__(self, cfg):
-        """
-        Create/load a MeTA inverted index based on the provided config file and
-        set the default ranking algorithm to Okapi BM25.
-        """
-        self.idx = metapy.index.make_inverted_index(cfg)
-        print(self.idx.num_docs())
-        print(self.idx.unique_terms())
-        self.default_ranker = metapy.index.OkapiBM25()
-        self.cfg = cfg
-
-    def load_ranker(self, ranker):
-        '''
-        :param ranker: ranker object from db
-        :return: meta.index.ranker
-        '''
-        ranker_id = ranker.ranker_id
-        # print(type(ranker_id))
-        try:
-            ranker = getattr(metapy.index, ranker_id)()
-            print('made ranker', ranker_id)
-        except:
-            print("Couldn't make '{}' ranker, using default.".format(ranker_id))
-            ranker = self.default_ranker
-        return ranker
-
-    def search(self, request, ranker):
-        """
-        Accept a JSON request and run the provided query with the specified
-        ranker.
-        """
-        # start = time.time()
-        # query = metapy.index.Document()
-        # query.content(str(request.POST.get('query_text', None)))
-        # # ranker_id = request.POST.get('ranker_id', None)
-        # ranker = self.load_ranker(ranker)
-        # ranker = self.default_ranker
-        # response = {'query': request.POST.get('query_text', None), 'results': []}
-        # print('response')
-        # import pdb
-        # pdb.set_trace()
-        # results = ranker.score(self.idx, query)
-        # # print(self.idx.num_docs())
-        # # print(self.idx.unique_terms())
-        # print(len(results))
-        # for result in results:
-        #     # print(result[1])
-        #     # print(self.idx.doc_path(result[0]))
-        #     response['results'].append({
-        #         'score': float(result[1]),
-        #         'name': self.idx.doc_name(result[0]),
-        #         'path': self.idx.doc_path(result[0])
-        #     })
-        # response['elapsed_time'] = time.time() - start
-        # return json.dumps(response, indent=2)
-
-    def eval(self, request, ranker):
-
-        ev = metapy.index.IREval(self.cfg)
-
-        with open(self.cfg, 'r') as fin:
-            cfg_d = pytoml.load(fin)
-
-        query_cfg = cfg_d['query-runner']
-
-        start_time = time.time()
-        top_k = 10
-        query_path = query_cfg.get('query-path', 'queries.txt')
-        query_start = query_cfg.get('query-id-start', 0)
-
-        query = metapy.index.Document()
-        ranker = self.load_ranker(ranker)
-        print('Running queries')
-        with open(query_path) as query_file:
-            for query_num, line in enumerate(query_file):
-                query.content(line.strip())
-                print(line.strip())
-                results = ranker.score(self.idx, query, top_k)
-                avg_p = ev.avg_p(results, query_start + query_num, top_k)
-                print("Query {} average precision: {}".format(query_num + 1, avg_p))
-        print("Mean average precision: {}".format(ev.map()))
-        print("Elapsed: {} seconds".format(round(time.time() - start_time, 4)))
