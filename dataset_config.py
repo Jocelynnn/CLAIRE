f8add38418bc208c522afadfbdec43f9f617e324
Aaron Green <aaron.green281@gmail.com>
2018-04-05 16:10:51 -0500

    change index path to use pre-constructed

--- dataset_config.py
+++ dataset_config.py
@@ -2,7 +2,7 @@ CONFIG = {
     "cranfield": {
         "name": "cranfield",
         "corpus": "line.toml",
-        "index": "cranfield-index",
+        "index": "/data/idx/cranfield-idx",
         "query-judgements": "/data/cranfield/cranfield-qrels.txt",
         "query-path": "/data/cranfield/cranfield-queries.txt",
         "stopwords": "lemur-stopwords.txt",
@@ -11,7 +11,7 @@ CONFIG = {
     "apnews": {
         "name": "ap88-89",
         "corpus": "gz.toml",
-        "index": "apnews-index",
+        "index": "/data/idx/ap88-89-idx",
         "query-judgements": "/data/ap88-89/qrels/qrels.1-150",
         "query-path": "/data/ap88-89/queries/topics.1-150.short-keyword",
         "stopwords": "lemur-stopwords.txt",
@@ -20,7 +20,7 @@ CONFIG = {
     "trec": {
         "name": "trec-robust",
         "corpus": "gz.toml",
-        "index": "trec-index",
+        "index": "/data/idx/trec-robust-idx",
         "query-judgements": "/data/trec-robust/trec-qrel",
         "query-path": "/data/trec-robust/trec-queries",
         "stopwords": "lemur-stopwords.txt",
